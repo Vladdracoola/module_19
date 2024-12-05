0c985e33bb8e55033411f5088cafb4ea0237bfa0
Vladdracoola <vladislav_pv@mail.ru>
2024-12-05 20:12:14 +0700

    1

--- Finish_line/Finish_line/settings.py
+++ Finish_line/Finish_line/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-&650*m9hd3sl_@h&v)y1!8zx9wfzs-_!o6ebiqd_!xwi7(nomf
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Finish_line.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -78,9 +75,16 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    'postgres': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'New DB',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -100,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -111,7 +114,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
--- Finish_line/task1/models.py
+++ Finish_line/task1/models.py
@@ -18,7 +18,7 @@ class Game(models.Model):
     cost = models.DecimalField(decimal_places=2, max_digits=99, default=1000)
     size = models.DecimalField(decimal_places=2, max_digits=99)
     description = models.TextField(default='Very good game, bro!')
-    age_limited = models.PositiveIntegerField(default=18)
+    age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='games')
 
     def __str__(self):
@@ -29,3 +29,21 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+class New_table(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50, null=False)
+    password = models.CharField(max_length=50, null=False)
+
+    class Meta:
+        db_table = 'New_table'
+        managed = False
+
+class Things(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50, null=False)
+    price = models.IntegerField(null=False, default=0)
+
+    class Meta:
+        db_table = 'things'
+        managed = False
